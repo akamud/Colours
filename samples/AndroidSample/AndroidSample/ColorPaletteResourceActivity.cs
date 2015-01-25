afa6d8aa90ae9b76708fff99f7bff315ead68fe9
Mahmoud Ali <muddibr@gmail.com>
2015-01-25 12:35:39 -0300

    Android sample project added showing all the colors available

---
+++ samples/AndroidSample/AndroidSample/ColorPaletteResourceActivity.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidSample
+{
+	[Activity (Label = "ColorPaletteResource")]			
+	public class ColorPaletteResourceActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.ColorPaletteResource);
+		}
+	}
+}
+
---
+++ samples/AndroidSample/AndroidSample/ColourPalette.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidSample
+{
+	public class ColourPalette : LinearLayout
+	{
+		public ColourPalette (Context context) :
+			base (context)
+		{
+			Initialize (context);
+		}
+
+		public ColourPalette (Context context, IAttributeSet attrs) :
+			base (context, attrs)
+		{
+			Initialize (context);
+		}
+
+		void Initialize (Context context)
+		{
+			View.Inflate(context, Resource.Layout.ColourPalette, this);
+		}
+	}
+}
+
