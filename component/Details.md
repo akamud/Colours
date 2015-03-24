cd2abe05e30b76f2a1e38f235bd7618ecd0438f8
Mahmoud Ali <muddibr@gmail.com>
2015-03-23 20:11:16 -0400

    Added images and details to send to the component store

--- component/Details.md
+++ component/Details.md
@@ -1,1 +1,23 @@
-# Colours details
+Colours
+================
+
+![Colours](https://raw.githubusercontent.com/akamud/Colours/master/images/colours.png)
+
+Colours is a port of the Colours Library for iOS made by [**Ben Gordon**](https://github.com/bennyguitar). You can find that project [**here**](https://github.com/bennyguitar/Colours). 
+This is also based on the Java port by [**Matthew York**](https://github.com/MatthewYork) found [**here**](https://github.com/MatthewYork/Colours).
+
+I just started this project as a way to have this on Xamarin Platforms, right now I'm focusing on the Xamarin.Android port. I plan to port it to Xamarin.Forms later. Since I don't own neither a Xamarin.iOS license or a Mac, I don't have any plans to port this to Xamarin.iOS. If you can contribute to that, I'll happily accept pull requests.
+
+## Android Sample
+
+The Android Sample contains various examples of how to use Colours in your project. You can also see the Color Palette available.
+
+![Android Sample](https://raw.githubusercontent.com/akamud/Colours/master/images/androidsample_screenshot_compressed.png)
+
+## Credits
+
+* [**Ben Gordon**](https://github.com/bennyguitar) - Author of the original iOS version of Colours
+* [**Matthew York**](https://github.com/MatthewYork) - Author of Colours for Android 
+
+## License
+[MIT License](https://github.com/akamud/Colours/blob/master/LICENSE)
--- src/Colours.Android/Resources/Resource.designer.cs
+++ src/Colours.Android/Resources/Resource.designer.cs
@@ -37,11 +37,414 @@ namespace Colours.Android
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f02005f
+			public static int almondColor = 2130837599;
+			
+			// aapt resource value: 0x7f020005
+			public static int antiqueWhiteColor = 2130837509;
+			
+			// aapt resource value: 0x7f02001d
+			public static int babyBlueColor = 2130837533;
+			
+			// aapt resource value: 0x7f020065
+			public static int background_dark = 2130837605;
+			
+			// aapt resource value: 0x7f02006a
+			public static int background_holo_dark = 2130837610;
+			
+			// aapt resource value: 0x7f02006b
+			public static int background_holo_light = 2130837611;
+			
+			// aapt resource value: 0x7f020066
+			public static int background_light = 2130837606;
+			
+			// aapt resource value: 0x7f02004c
+			public static int bananaColor = 2130837580;
+			
+			// aapt resource value: 0x7f020053
+			public static int beigeColor = 2130837587;
+			
+			// aapt resource value: 0x7f02000f
+			public static int black = 2130837519;
+			
+			// aapt resource value: 0x7f02000c
+			public static int black25PercentColor = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public static int black50PercentColor = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public static int black75PercentColor = 2130837518;
+			
+			// aapt resource value: 0x7f02001b
+			public static int blueberryColor = 2130837531;
+			
+			// aapt resource value: 0x7f020031
+			public static int brickRedColor = 2130837553;
+			
+			// aapt resource value: 0x7f020067
+			public static int bright_foreground_dark = 2130837607;
+			
+			// aapt resource value: 0x7f020069
+			public static int bright_foreground_dark_disabled = 2130837609;
+			
+			// aapt resource value: 0x7f02006e
+			public static int bright_foreground_disabled_holo_dark = 2130837614;
+			
+			// aapt resource value: 0x7f02006f
+			public static int bright_foreground_disabled_holo_light = 2130837615;
+			
+			// aapt resource value: 0x7f02006c
+			public static int bright_foreground_holo_dark = 2130837612;
+			
+			// aapt resource value: 0x7f02006d
+			public static int bright_foreground_holo_light = 2130837613;
+			
+			// aapt resource value: 0x7f020068
+			public static int bright_foreground_light = 2130837608;
+			
+			// aapt resource value: 0x7f020055
+			public static int burntOrangeColor = 2130837589;
+			
+			// aapt resource value: 0x7f02005b
+			public static int burntSiennaColor = 2130837595;
+			
+			// aapt resource value: 0x7f02004e
+			public static int buttermilkColor = 2130837582;
+			
+			// aapt resource value: 0x7f020027
+			public static int cactusGreenColor = 2130837543;
+			
+			// aapt resource value: 0x7f020057
+			public static int cantaloupeColor = 2130837591;
+			
+			// aapt resource value: 0x7f02002f
+			public static int cardTableColor = 2130837551;
+			
+			// aapt resource value: 0x7f020058
+			public static int carrotColor = 2130837592;
+			
+			// aapt resource value: 0x7f020012
+			public static int charcoalColor = 2130837522;
+			
+			// aapt resource value: 0x7f020028
+			public static int chartreuseColor = 2130837544;
+			
+			// aapt resource value: 0x7f02005a
+			public static int chiliPowderColor = 2130837594;
+			
+			// aapt resource value: 0x7f02005c
+			public static int chocolateColor = 2130837596;
+			
+			// aapt resource value: 0x7f02005e
+			public static int cinnamonColor = 2130837598;
+			
+			// aapt resource value: 0x7f02005d
+			public static int coffeeColor = 2130837597;
+			
+			// aapt resource value: 0x7f020011
+			public static int coolGrayColor = 2130837521;
+			
+			// aapt resource value: 0x7f020041
+			public static int coolPurpleColor = 2130837569;
+			
+			// aapt resource value: 0x7f020037
+			public static int coralColor = 2130837559;
+			
+			// aapt resource value: 0x7f02001c
+			public static int cornflowerColor = 2130837532;
+			
+			// aapt resource value: 0x7f020050
+			public static int creamColor = 2130837584;
+			
+			// aapt resource value: 0x7f02003d
+			public static int crimsonColor = 2130837565;
+			
+			// aapt resource value: 0x7f020003
+			public static int dangerColor = 2130837507;
+			
+			// aapt resource value: 0x7f02001a
+			public static int denimColor = 2130837530;
+			
+			// aapt resource value: 0x7f020071
+			public static int dim_foreground_disabled_holo_dark = 2130837617;
+			
+			// aapt resource value: 0x7f020074
+			public static int dim_foreground_disabled_holo_light = 2130837620;
+			
+			// aapt resource value: 0x7f020070
+			public static int dim_foreground_holo_dark = 2130837616;
+			
+			// aapt resource value: 0x7f020073
+			public static int dim_foreground_holo_light = 2130837619;
+			
+			// aapt resource value: 0x7f020064
+			public static int dustColor = 2130837604;
+			
+			// aapt resource value: 0x7f020032
+			public static int easterPinkColor = 2130837554;
+			
+			// aapt resource value: 0x7f02003e
+			public static int eggplantColor = 2130837566;
+			
+			// aapt resource value: 0x7f020060
+			public static int eggshellColor = 2130837600;
+			
+			// aapt resource value: 0x7f020022
+			public static int emeraldColor = 2130837538;
+			
+			// aapt resource value: 0x7f02001f
+			public static int fadedBlueColor = 2130837535;
+			
+			// aapt resource value: 0x7f020046
+			public static int fuschiaColor = 2130837574;
+			
+			// aapt resource value: 0x7f020009
+			public static int ghostWhiteColor = 2130837513;
+			
+			// aapt resource value: 0x7f02004f
+			public static int goldColor = 2130837583;
+			
+			// aapt resource value: 0x7f02004a
+			public static int goldenrodColor = 2130837578;
+			
+			// aapt resource value: 0x7f020047
+			public static int grapeColor = 2130837575;
+			
+			// aapt resource value: 0x7f020033
+			public static int grapefruitColor = 2130837555;
+			
+			// aapt resource value: 0x7f020023
+			public static int grassColor = 2130837539;
+			
+			// aapt resource value: 0x7f020076
+			public static int highlighted_text_holo_dark = 2130837622;
+			
+			// aapt resource value: 0x7f020077
+			public static int highlighted_text_holo_light = 2130837623;
+			
+			// aapt resource value: 0x7f020072
+			public static int hint_foreground_holo_dark = 2130837618;
+			
+			// aapt resource value: 0x7f020075
+			public static int hint_foreground_holo_light = 2130837621;
+			
+			// aapt resource value: 0x7f020029
+			public static int hollyGreenColor = 2130837545;
+			
+			// aapt resource value: 0x7f020081
+			public static int holo_blue_bright = 2130837633;
+			
+			// aapt resource value: 0x7f02007b
+			public static int holo_blue_dark = 2130837627;
+			
+			// aapt resource value: 0x7f020078
+			public static int holo_blue_light = 2130837624;
+			
+			// aapt resource value: 0x7f02007c
+			public static int holo_green_dark = 2130837628;
+			
+			// aapt resource value: 0x7f020079
+			public static int holo_green_light = 2130837625;
+			
+			// aapt resource value: 0x7f020080
+			public static int holo_orange_dark = 2130837632;
+			
+			// aapt resource value: 0x7f02007f
+			public static int holo_orange_light = 2130837631;
+			
+			// aapt resource value: 0x7f02007e
+			public static int holo_purple = 2130837630;
+			
+			// aapt resource value: 0x7f02007d
+			public static int holo_red_dark = 2130837629;
+			
+			// aapt resource value: 0x7f02007a
+			public static int holo_red_light = 2130837626;
+			
+			// aapt resource value: 0x7f02002d
+			public static int honeydewColor = 2130837549;
+			
+			// aapt resource value: 0x7f020020
+			public static int icebergColor = 2130837536;
+			
+			// aapt resource value: 0x7f020035
+			public static int indianRedColor = 2130837557;
+			
+			// aapt resource value: 0x7f020019
+			public static int indigoColor = 2130837529;
+			
+			// aapt resource value: 0x7f020000
+			public static int infoBlueColor = 2130837504;
+			
+			// aapt resource value: 0x7f020007
+			public static int ivoryColor = 2130837511;
+			
+			// aapt resource value: 0x7f020044
+			public static int lavenderColor = 2130837572;
+			
+			// aapt resource value: 0x7f020051
+			public static int lightCreamColor = 2130837585;
+			
+			// aapt resource value: 0x7f02002e
+			public static int limeColor = 2130837550;
+			
+			// aapt resource value: 0x7f02000b
+			public static int linenColor = 2130837515;
+			
+			// aapt resource value: 0x7f020059
+			public static int mandarinColor = 2130837593;
+			
+			// aapt resource value: 0x7f020038
+			public static int maroonColor = 2130837560;
+			
+			// aapt resource value: 0x7f02001e
+			public static int midnightBlueColor = 2130837534;
+			
+			// aapt resource value: 0x7f02002c
+			public static int moneyGreenColor = 2130837548;
+			
+			// aapt resource value: 0x7f020062
+			public static int mudColor = 2130837602;
+			
+			// aapt resource value: 0x7f02004d
+			public static int mustardColor = 2130837581;
+			
+			// aapt resource value: 0x7f020006
+			public static int oldLaceColor = 2130837510;
+			
+			// aapt resource value: 0x7f02002a
+			public static int oliveColor = 2130837546;
+			
+			// aapt resource value: 0x7f02002b
+			public static int oliveDrabColor = 2130837547;
+			
+			// aapt resource value: 0x7f020049
+			public static int orchidColor = 2130837577;
+			
+			// aapt resource value: 0x7f020026
+			public static int paleGreenColor = 2130837542;
+			
+			// aapt resource value: 0x7f020040
+			public static int palePurpleColor = 2130837568;
+			
+			// aapt resource value: 0x7f02003c
+			public static int paleRoseColor = 2130837564;
+			
+			// aapt resource value: 0x7f020016
+			public static int pastelBlueColor = 2130837526;
+			
+			// aapt resource value: 0x7f020024
+			public static int pastelGreenColor = 2130837540;
+			
+			// aapt resource value: 0x7f020056
+			public static int pastelOrangeColor = 2130837590;
+			
+			// aapt resource value: 0x7f02003f
+			public static int pastelPurpleColor = 2130837567;
+			
+			// aapt resource value: 0x7f020054
+			public static int peachColor = 2130837588;
+			
+			// aapt resource value: 0x7f020048
+			public static int periwinkleColor = 2130837576;
+			
+			// aapt resource value: 0x7f020034
+			public static int pinkColor = 2130837556;
+			
+			// aapt resource value: 0x7f02003b
+			public static int pinkLipstickColor = 2130837563;
+			
+			// aapt resource value: 0x7f020043
+			public static int plumColor = 2130837571;
+			
+			// aapt resource value: 0x7f020045
+			public static int raspberryColor = 2130837573;
+			
+			// aapt resource value: 0x7f020015
+			public static int robinEggColor = 2130837525;
+			
+			// aapt resource value: 0x7f020030
+			public static int salmonColor = 2130837552;
+			
+			// aapt resource value: 0x7f020061
+			public static int sandColor = 2130837601;
+			
+			// aapt resource value: 0x7f020025
+			public static int seafoamColor = 2130837541;
+			
+			// aapt resource value: 0x7f020008
+			public static int seashellColor = 2130837512;
+			
+			// aapt resource value: 0x7f020063
+			public static int siennaColor = 2130837603;
+			
+			// aapt resource value: 0x7f020018
+			public static int skyBlueColor = 2130837528;
+			
+			// aapt resource value: 0x7f02000a
+			public static int snowColor = 2130837514;
+			
+			// aapt resource value: 0x7f020014
+			public static int steelBlueColor = 2130837524;
+			
+			// aapt resource value: 0x7f020036
+			public static int strawberryColor = 2130837558;
+			
+			// aapt resource value: 0x7f020001
+			public static int successColor = 2130837505;
+			
+			// aapt resource value: 0x7f020013
+			public static int tealColor = 2130837523;
+			
+			// aapt resource value: 0x7f02003a
+			public static int tomatoColor = 2130837562;
+			
+			// aapt resource value: 0x7f020017
+			public static int turquoiseColor = 2130837527;
+			
+			// aapt resource value: 0x7f020042
+			public static int violetColor = 2130837570;
+			
+			// aapt resource value: 0x7f020010
+			public static int warmGrayColor = 2130837520;
+			
+			// aapt resource value: 0x7f020002
+			public static int warningColor = 2130837506;
+			
+			// aapt resource value: 0x7f020039
+			public static int watermelonColor = 2130837561;
+			
+			// aapt resource value: 0x7f020021
+			public static int waveColor = 2130837537;
+			
+			// aapt resource value: 0x7f020052
+			public static int wheatColor = 2130837586;
+			
+			// aapt resource value: 0x7f020004
+			public static int white = 2130837508;
+			
+			// aapt resource value: 0x7f02004b
+			public static int yellowGreenColor = 2130837579;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f020000
-			public static int library_name = 2130837504;
+			// aapt resource value: 0x7f030000
+			public static int library_name = 2130903040;
 			
 			static String()
 			{
